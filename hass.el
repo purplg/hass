d8cfd1c0449a454b7e8d20e5433ddba3e3a22382
Ben Whitley <dev@purplg.com>
2021-09-03 21:03:15 -0400

    version bump

--- hass.el
+++ hass.el
@@ -1,7 +1,7 @@
 ;;; hass.el --- Interact with Home Assistant -*- lexical-binding: t; -*-
 
 ;; Package-Requires: ((emacs "25.1") (request "0.3.3"))
-;; Version: 0.1
+;; Version: 1.0
 ;; Author: Ben Whitley
 ;;; Commentary:
 
