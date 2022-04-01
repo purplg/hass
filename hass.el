44bb81d9d1b5263d6fbcd52c708bc81dcca935bb
Ben Whitley <dev@purplg.com>
2022-04-01 18:22:33 -0400

    Updated readme to include dashboard and websockets information

--- hass.el
+++ hass.el
@@ -1,7 +1,7 @@
 ;;; hass.el --- Interact with Home Assistant -*- lexical-binding: t; -*-
 
 ;; Package-Requires: ((emacs "25.1") (request "0.3.3"))
-;; Version: 2.0.0
+;; Version: 2.1.1
 ;; Author: Ben Whitley
 ;; Homepage: https://github.com/purplg/hass
 ;; SPDX-License-Identifier: MIT
