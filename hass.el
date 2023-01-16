033d11b07e0f8bb68b348a12d5ae13fabea56c73
Ben Whitley <dev@purplg.com>
2023-01-16 17:20:09 -0500

    Version bump

--- hass.el
+++ hass.el
@@ -1,7 +1,7 @@
 ;;; hass.el --- Interact with Home Assistant -*- lexical-binding: t; -*-
 
 ;; Package-Requires: ((emacs "25.1") (request "0.3.3") (websocket "1.13"))
-;; Version: 3.0.1
+;; Version: 3.0.2
 ;; Author: Ben Whitley
 ;; Homepage: https://github.com/purplg/hass
 ;; SPDX-License-Identifier: MIT
